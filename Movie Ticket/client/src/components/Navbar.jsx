7ef1e08b070aa2d1b63125871223ccc235513318
Sumitg2803 <sumit.s.gaikwad28@gmail.com>
2025-10-25 21:31:25 +0530

    Login setup error solved

--- Movie Ticket/client/src/components/Navbar.jsx
+++ Movie Ticket/client/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
 
   const [isOpen, setIsOpen] =  useState(false);
   const {user} = useUser();
-  const {OpenSignIn} = useClerk();
+  const {openSignIn} = useClerk();
 
     return (
         <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 md:px-16 lg:px-36 py-5'>
@@ -32,7 +32,7 @@ const Navbar = () => {
             <SearchIcon className='max-md:hidden w-6 h-6 cursor-pointer'/>
             {
                 !user ? (
-                    <button onClick={OpenSignIn} className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>Login</button>
+                    <button onClick={openSignIn} className='px-4 py-1 sm:px-7 sm:py-2 bg-primary hover:bg-primary-dull transition rounded-full font-medium cursor-pointer'>Login</button>
                 ) : (
                     <UserButton />
                 )
