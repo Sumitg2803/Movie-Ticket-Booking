3980bfc174f8fe3f5a20dc944f695143aadda9eb
Sumitg2803 <sumit.s.gaikwad28@gmail.com>
2025-10-31 21:15:30 +0530

    all file are formated into Prettier Version

--- Movie Ticket/client/src/pages/Home.jsx
+++ Movie Ticket/client/src/pages/Home.jsx
@@ -1,16 +1,16 @@
-import React from 'react';
-import HeroSection from '../components/HeroSection';
-import FeaturedSection from '../components/FeaturedSection';
-import TrailersSection from '../components/TrailerSection';
+import React from "react";
+import HeroSection from "../components/HeroSection";
+import FeaturedSection from "../components/FeaturedSection";
+import TrailersSection from "../components/TrailerSection";
 
 const Home = () => {
-    return (
-        <>
-            <HeroSection />
-            <FeaturedSection />
-            <TrailersSection />
-        </>
-    );
-}
+  return (
+    <>
+      <HeroSection />
+      <FeaturedSection />
+      <TrailersSection />
+    </>
+  );
+};
 
 export default Home;
--- Movie Ticket/client/src/pages/SeatLayout.jsx
+++ Movie Ticket/client/src/pages/SeatLayout.jsx
@@ -1,108 +1,122 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate, useParams } from 'react-router-dom'
-import { dummyShowsData, dummyDateTimeData, assets } from '../assets/assets'
-import Loading from '../components/Loading'
-import { ClockIcon } from 'lucide-react'
-import isoTimeFormat from '../lib/isoTimeFormat'
-import BlurCircle from '../components/BlurCircle'
-import toast from 'react-hot-toast'
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { dummyShowsData, dummyDateTimeData, assets } from "../assets/assets";
+import Loading from "../components/Loading";
+import { ClockIcon } from "lucide-react";
+import isoTimeFormat from "../lib/isoTimeFormat";
+import BlurCircle from "../components/BlurCircle";
+import toast from "react-hot-toast";
 
 const SeatLayout = () => {
+  const groupRows = [
+    ["A", "B"],
+    ["C", "D"],
+    ["E", "F"],
+    ["G", "H"],
+    ["I", "J"],
+  ];
 
-  const groupRows = [["A", "B"], ["C", "D"], ["E", "F"], ["G", "H"], ["I", "J"]]
-
-
-  const { id, date } = useParams()
-  const [selectedSeats, setSelectedSeats] = useState([])
-  const [selectedTime, setSelectedTime] = useState(null)
-  const [show, setShow] = useState(null)
-  const navigate = useNavigate()
+  const { id, date } = useParams();
+  const [selectedSeats, setSelectedSeats] = useState([]);
+  const [selectedTime, setSelectedTime] = useState(null);
+  const [show, setShow] = useState(null);
+  const navigate = useNavigate();
 
   const getShow = async () => {
-    const showData = dummyShowsData.find(s => String(s._id) === String(id)) // <— if your id key is `_id`, use that
+    const showData = dummyShowsData.find((s) => String(s._id) === String(id)); // <— if your id key is `_id`, use that
     if (showData) {
       setShow({
         movie: showData,
-        dateTime: dummyDateTimeData
-      })
+        dateTime: dummyDateTimeData,
+      });
     }
-  }
+  };
 
   const handleSeatClick = (seatId) => {
     if (!selectedTime) {
-      return toast("Please select a show time first")
+      return toast("Please select a show time first");
     }
     if (!selectedSeats.includes(seatId) && selectedSeats.length >= 4) {
-      return toast("You can only select up to 4 seats")
+      return toast("You can only select up to 4 seats");
     }
-    setSelectedSeats(prev => prev.includes(seatId) ? prev.filter(seat => seat !== seatId) : [...prev, seatId])
-  }
+    setSelectedSeats((prev) =>
+      prev.includes(seatId)
+        ? prev.filter((seat) => seat !== seatId)
+        : [...prev, seatId]
+    );
+  };
   const renderseats = (row, count = 9) => (
-
     <div key={row} className="flex gap-2 mt-2">
-    <div className="flex flex-wrap items-center justify-center gap-2">
-      {Array.from({ length: count }, (_, i) => {
-        const seatId = `${row}${i + 1}`;
-        return (
-          <button 
-            key={seatId} 
-            onClick={() => handleSeatClick(seatId)} 
-            className={`h-8 w-8 rounded border border-primary/60 cursor-pointer ${selectedSeats.includes(seatId) && "bg-primary text-white"}`}
-          >
-            {seatId}
-          </button>
-        );
-      })}
+      <div className="flex flex-wrap items-center justify-center gap-2">
+        {Array.from({ length: count }, (_, i) => {
+          const seatId = `${row}${i + 1}`;
+          return (
+            <button
+              key={seatId}
+              onClick={() => handleSeatClick(seatId)}
+              className={`h-8 w-8 rounded border border-primary/60 cursor-pointer ${
+                selectedSeats.includes(seatId) && "bg-primary text-white"
+              }`}
+            >
+              {seatId}
+            </button>
+          );
+        })}
+      </div>
     </div>
-  </div>
+  );
 
-    )
-
-  useEffect(() => { getShow() }, [id])
+  useEffect(() => {
+    getShow();
+  }, [id]);
 
   return show ? (
-    <div className='flex flex-col md:flex-row px-6 md:px-16 lg:px-40 py-30 md:pt-50'>
-        {/*Available Timings*/}
-        <div className='w-60 bg-primary/10 border border-primary/20 rounded-lg py-10 h-max md:sticky md:top-30'>
-            <p className='text-lg font-semibold px-6'>Available Timings</p>
-            <div className='mt-5 space-y-1'>
-                {show.dateTime[date].map((time)=>(
-                    <div key={time.time} onClick={()=> setSelectedTime(time)} className= {`flex items-center gap-2 px-6 py-2 w-max rounded-r-md cursor-pointer transition ${selectedTime?.time === time.time ? "bg-primary text-white" : "hover:bg-primary/20"}`}> 
-                        <ClockIcon className="w-4 h-4" />
-                        <p className='text-sm'>{isoTimeFormat(time.time)}</p>
-                    </div>
-                ))}
-            </div>
-        </div>
-
-        {/*Seat Layout*/}
-        <div className='relative flex-1 flex flex-col items-center max-md:mt-16'>
-            <BlurCircle top="-100px" left="-100px"/>
-            <BlurCircle bottom="0" right="0"/>
-            <h1 className='text-2xl font-semibold mb-4'>Select your seat</h1>
-            <img src={assets.screenImage} alt="screen"/>
-            <p className='text-gray-400 text-sm mb-6'>SCREEN SIDE</p>
-
-            <div className='flex flex-col items-center mt-10 text-xs text-gray-300'>
-              <div className='grid grid-cols-2 md:grid-cols-1 gap-8 md:gap-2 mb-6'>
-                {groupRows[0].map(row => renderseats(row))}
-              </div>
-            </div>
-
-            <div className='grid grid-cols-2 gap-11'>
-          {groupRows.slice(1).map((group, idx)=>(
-            <div key={idx}>
-              {group.map(row => renderseats(row))}
+    <div className="flex flex-col md:flex-row px-6 md:px-16 lg:px-40 py-30 md:pt-50">
+      {/*Available Timings*/}
+      <div className="w-60 bg-primary/10 border border-primary/20 rounded-lg py-10 h-max md:sticky md:top-30">
+        <p className="text-lg font-semibold px-6">Available Timings</p>
+        <div className="mt-5 space-y-1">
+          {show.dateTime[date].map((time) => (
+            <div
+              key={time.time}
+              onClick={() => setSelectedTime(time)}
+              className={`flex items-center gap-2 px-6 py-2 w-max rounded-r-md cursor-pointer transition ${
+                selectedTime?.time === time.time
+                  ? "bg-primary text-white"
+                  : "hover:bg-primary/20"
+              }`}
+            >
+              <ClockIcon className="w-4 h-4" />
+              <p className="text-sm">{isoTimeFormat(time.time)}</p>
             </div>
           ))}
         </div>
-
       </div>
 
-        
+      {/*Seat Layout*/}
+      <div className="relative flex-1 flex flex-col items-center max-md:mt-16">
+        <BlurCircle top="-100px" left="-100px" />
+        <BlurCircle bottom="0" right="0" />
+        <h1 className="text-2xl font-semibold mb-4">Select your seat</h1>
+        <img src={assets.screenImage} alt="screen" />
+        <p className="text-gray-400 text-sm mb-6">SCREEN SIDE</p>
 
+        <div className="flex flex-col items-center mt-10 text-xs text-gray-300">
+          <div className="grid grid-cols-2 md:grid-cols-1 gap-8 md:gap-2 mb-6">
+            {groupRows[0].map((row) => renderseats(row))}
+          </div>
+        </div>
+
+        <div className="grid grid-cols-2 gap-11">
+          {groupRows.slice(1).map((group, idx) => (
+            <div key={idx}>{group.map((row) => renderseats(row))}</div>
+          ))}
+        </div>
+      </div>
     </div>
-  ) : <Loading />
-}
+  ) : (
+    <Loading />
+  );
+};
 
-export default SeatLayout
+export default SeatLayout;
